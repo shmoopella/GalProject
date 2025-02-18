7d155e2a6e08a181c1c9f64edbeb6db0cc7f31fa
Shmupella <emilynovael@icloud.com>
2025-02-18 16:59:24 +0300

    ui fix

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+const realInput = document.querySelector("#real-input");
+const customButton = document.querySelector("#custom-button");
+const fileName = document.querySelector("#file-name");
+
+// Обработчик клика на кастомную кнопку
+customButton.addEventListener("click", () => {
+  realInput.click(); // Имитируем клик на реальном input
+});
+
+// Обновление надписи при выборе файла
+realInput.addEventListener("change", function () {
+  if (this.files.length > 0) {
+    fileName.textContent = this.files[0].name;
+  } else {
+    fileName.textContent = "Файл не выбран";
+  }
+});
+
 function convertFile() {
-  const fileInput = document.querySelector("#fileInput");
+  const fileInput = document.querySelector("#real-input");
   const file = fileInput.files[0];
 
   if (!file) {
@@ -31,7 +49,7 @@ function convertFile() {
     const blob = new Blob([newStr], { type: "text/plain" });
     const link = document.createElement("a");
     link.href = URL.createObjectURL(blob);
-    link.download = "converted_" + file.name; // Имя файла
+    link.download = "converted_" + file.name;
     link.click();
   };
   reader.readAsText(file);
